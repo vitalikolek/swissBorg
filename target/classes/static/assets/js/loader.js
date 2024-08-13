1942c9edae0fcc2e587b4ea478c63769d4cd6a4e
vitalikolek <103362692+vitalikolek@users.noreply.github.com>
2024-08-13 20:01:21 +0200

    Delete target directory

--- target/classes/static/assets/js/loader.js
+++
@@ -1,6 +0,0 @@
-const iframe = document.getElementById("rs-buy-sell-iframe")
-const iframeLoader = document.querySelector(".rs-buy-sell__loader")
-
-iframe.addEventListener("load", () => {
-    iframeLoader.classList.add("disable")
-})
